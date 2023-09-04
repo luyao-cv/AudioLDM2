441766f40975d1df68eb84a65553f73e171da69c
luayo-cv <1367355728@qq.com>
2023-09-04 06:31:25 +0000

    add image to music demo

---
+++ test_audio.py
@@ -0,0 +1,128 @@
+import torch
+import scipy
+
+import gradio as gr
+import torch
+from diffusers import AudioLDM2Pipeline
+import openai
+import gradio as gr
+from gradio.components import Textbox
+import gradio as gr
+#输入文本处理程序
+import numpy as np
+import scipy
+from PIL import Image
+import requests
+import json
+
+# make Space compatible with CPU duplicates
+if torch.cuda.is_available():
+    device = "cuda"
+    torch_dtype = torch.float16
+else:
+    device = "cpu"
+    torch_dtype = torch.float32
+
+# load the diffusers pipeline
+repo_id = "cvssp/audioldm2"
+pipe = AudioLDM2Pipeline.from_pretrained(repo_id, torch_dtype=torch_dtype).to(device)
+
+# set the generator for reproducibility
+generator = torch.Generator(device)
+
+def text2audio(x, result_blip, prompt, num_inference_steps, negative_textbox, duration, guidance_scale, random_seed, n_candidates):
+    if x is None:
+        raise gr.Error("Please provide a image input.")
+    
+    
+    img_path = "tmp.png"
+    x.save(img_path)
+    
+    # img2prompt(_path, result_blip, prompt)
+
+    input_json = {
+        "path":img_path,
+        "result_blip":result_blip,
+        "prompt": prompt
+    }
+    
+    results = requests.post("http://10.21.226.179:8912/blip2", json=input_json)
+    # print(results.text)
+    result_json = json.loads(results.text)
+    
+    result_blip, prompt = result_json['result_blip'],result_json['prompt']
+    
+    waveforms = pipe(
+        prompt,
+        audio_length_in_s=duration,
+        guidance_scale=guidance_scale,
+        num_inference_steps=num_inference_steps,
+        negative_prompt=negative_textbox,
+        num_waveforms_per_prompt=n_candidates if n_candidates else 1,
+        generator=generator.manual_seed(int(random_seed)),
+    )["audios"]
+
+    return gr.make_waveform((16000, waveforms[0]), bg_image="bg.png"), result_blip, prompt, negative_textbox, num_inference_steps, duration
+
+iface = gr.Blocks()
+
+with iface:
+
+    with gr.Group():
+
+        image_input = gr.Image(type="pil")
+        
+        with gr.Column():
+            with gr.Column():
+                gr.Markdown(value="""
+                <font size=2> prompt输出, 可自行修改</font>
+                """)
+                Text_blip2 = gr.Textbox(label="blip2输出...")
+                Text_chatgpt = gr.Textbox(label="chatgpt输出...")
+                # gr.ClearButton([Text_blip2, Text_chatgpt], value="清空，不清空默认使用该prompt")
+
+
+        with gr.Column():
+            gr.Markdown(value="""
+            <font size=2> 参数设置</font>
+            """)
+
+            with gr.Row():
+                negative_textbox = gr.Textbox(label="negative prompt", value="low quality")
+
+
+        with gr.Accordion("Click to modify detailed configurations", open=False):
+            seed = gr.Number(
+                value=45,
+                label="Seed",
+                info="Change this value (any integer number) will lead to a different generation result.",
+            )
+            duration = gr.Slider(5, 15, value=10, step=2.5, label="Duration (seconds)")
+            guidance_scale = gr.Slider(
+                0,
+                7,
+                value=3.5,
+                step=0.5,
+                label="Guidance scale",
+                info="Larger => better quality and relevancy to text; Smaller => better diversity",
+            )
+            n_candidates = gr.Slider(
+                1,
+                5,
+                value=3,
+                step=1,
+                label="Number waveforms to generate",
+                info="Automatic quality control. This number control the number of candidates (e.g., generate three audios and choose the best to show you). A larger value usually lead to better quality with heavier computation",
+            )
+            num_inference_steps = gr.Slider(label="迭代步数，步数越大越接近扩散模型的结果", value=200, minimum = 1, maximum = 1000)
+
+        outputs = gr.Video(label="Output", elem_id="output-video")
+        btn = gr.Button("generate music")
+
+    btn.click(
+        text2audio,
+        inputs=[image_input, Text_blip2, Text_chatgpt, num_inference_steps, negative_textbox, duration, guidance_scale, seed, n_candidates],
+        outputs=[outputs, Text_blip2, Text_chatgpt, negative_textbox, num_inference_steps, duration]
+    )
+    
+iface.launch(share=True, server_name="0.0.0.0", server_port=8911)
---
+++ test_blip2.py
@@ -0,0 +1,97 @@
+import openai
+
+#输入文本处理程序
+import numpy as np
+from paddlemix import Appflow
+import paddle
+import scipy
+from PIL import Image
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+openai.api_key = "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"  # 更换为自己的密钥
+
+messages = [
+    {"role": "system", "content": "You are a helpful and kind AI Assistant."},
+]
+
+blip_to_ldm_prompt = """
+Create a brief description of a specific musical style that matches the following sentence.:
+"{}".
+
+Here are some output examples:
+
+examples 1: "A modern hip-hop beat with punchy bass and rapid-fire lyrics, reflecting urban life and culture."
+
+examples 2: "A funky bass guitar grooving in sync with drums."
+
+examples 3: "A cjeerful ukulele strumming in a beachside jam."
+
+examples 4: "Melodic pop song in trance progressive, award winning, epic sound, climax."
+
+examples 5: "A forest of wind chimes singing a soothing melody in the breeze."
+
+examples 6: "Birds singing sweetly in a blooming garden."
+
+examples 7: "A modern synthesizer ceating futuristic soundscapes."
+
+examples 8: "A traditional Irish fiddle playing a lively reel. "
+
+examples 9: "The enchanting sound of a harp being plucked."
+
+examples 10: "A saxophone weaves a soulful melody, accompanied by the iconic Super Mario music, creating a nostalgic fusion that merges smooth jazz with retro gaming vibes."
+
+examples 11: "The vibrant beat of Brazilian samba drums intertwines with a catchy trap beat, while EDM synthesizers add a unique electronic flair, resulting in a dynamic and energetic sound with an ethereal quality."
+
+examples 12: "Delicate harp arpeggios blend with angelic choir vocals, creating a heavenly and ethereal atmosphere that enchants and uplifts the spirit."
+
+examples 13: "Embark on a whimsical journey with the playful melodies and light-hearted rhythms of a delightful indie folk tune, capturing the spirit of a bounding deer in full stride." 
+
+examples 14: "Dance music with strong, upbeat tempo, and repetitive rhythms, include sub-genres like house, techno, EDM, trance, and many more."
+
+examples 15: "Scary music with dissonant harmonies, irregular rhythms, and unconventional use of instruments."
+
+examples 16: "Pop music that upbeat, catchy, and easy to listen, high fidelity, with simple melodies, electronic instruments and polished production."
+
+examples 17: "This is a piece that would be suitable as calming study music or music for sleeping. It features a relaxing and soothing motif on the piano, being backed by a distant, high pitched and sustained violin."
+
+"""
+def ChatGPT_Bot(input):
+    if input:
+        messages.append({"role": "user", "content": input})
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        return reply
+
+task_blip = Appflow(app="auto_label", models=["paddlemix/blip2-caption-opt2.7b"])
+# x = ChatGPT_Bot("你好")
+# print(x)
+
+def img2prompt(x, result_blip, prompt):
+    if not result_blip:
+        result_blip = task_blip(image=x)['prompt']
+        # result_blip = "test blip2"
+    if not prompt:
+        pre_prompt = blip_to_ldm_prompt.format(result_blip)
+        prompt = ChatGPT_Bot(pre_prompt)
+        # prompt = "test chatgpt"
+
+    print(result_blip)
+    print(prompt)
+    return result_blip, prompt
+
+@app.route("/blip2",methods=["POST"])
+def post_prompt():
+    input_json = request.get_json()
+    _path,result_blip, prompt = input_json['path'],input_json['result_blip'],input_json['prompt']
+    # '/paddle/luyao15/project/PaddleMIX/violin.jpg'
+    result_blip, prompt = img2prompt(_path, result_blip, prompt)
+    return dict(result_blip=result_blip, prompt=prompt)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=8912,debug=False)
